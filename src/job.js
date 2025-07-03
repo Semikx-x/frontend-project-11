492e074642c1affacf0f36d39646cf8bd558cd8f
Semikx-x <semen.samsonov@mail.ru>
2025-07-03 12:55:38 +0300

    fix

--- src/job.js
+++ src/job.js
@@ -8,9 +8,10 @@ async function update(links, state, i18n, wathedObject, p) {
 
 const timer = async () => new Promise(res => setTimeout(res, 5000))
 
-export default async (links, state, i18n, wathedObject, p) => {
-  while (true) {
+export default async (links, state, i18n, wathedObject, p, sholdExit = true) => {
+  for (;;) {
     await timer()
     update(links, state, i18n, wathedObject, p)
+    if (sholdExit === false) break
   }
 }
