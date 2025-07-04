fe02f3fa969dff9e8e46ac37df47339b5d1f9de8
Semikx-x <semen.samsonov@mail.ru>
2025-07-04 14:44:53 +0300

    fix

--- src/zapros.js
+++ src/zapros.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async (url, i18n) => {
   const instance = axios.create()
-  const response = await instance.get('https://allorigins.hexlet.app/get??disableCache=true&url=' + url, {
+  const response = await instance.get('https://allorigins.hexlet.app/get?disableCache=true&url=' + url, {
     timeout: 3000 })
   if (response.status === 200) {
     return response
