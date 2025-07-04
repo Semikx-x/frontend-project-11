ded1ded05590f1c5c82ea89086af108f3637a370
Semikx-x <semen.samsonov@mail.ru>
2025-07-04 15:17:45 +0300

    fix

--- src/zapros.js
+++ src/zapros.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 
 export default async (url, i18n) => {
-  const instance = axios.create()
-  const response = await instance.get('https://allorigins.hexlet.app/get?disableCache=true&url=' + url, {
-    timeout: 3000 })
-  if (response.status === 200) {
-    return response
+  try {
+    const instance = axios.create()
+    const response = await instance.get('https://allorigins.hexlet.app/get?disableCache=true&url=' + url, {
+      timeout: 3000 })
+    if (response.status === 200) {
+      return response
+    }
+  }
+  // eslint-disable-next-line no-unused-vars
+  catch (_) {
+    throw new Error(i18n.t('RssNull'))
   }
 
   throw new Error(i18n.t('networkErr'))
