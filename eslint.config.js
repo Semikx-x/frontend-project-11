67ee438d34387368ab8d2054087516fcc9e91d7c
Semikx-x <semen.samsonov@mail.ru>
2025-07-05 13:36:44 +0300

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-
 import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
@@ -6,6 +5,7 @@ import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   stylistic.configs.recommended,
+  { ignores: ['dist/**', 'hexlet-project-11/**'] },
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 ])
